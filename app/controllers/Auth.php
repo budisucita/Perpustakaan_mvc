d9d64d2d8924f234c67f2ba837ca4ea858d74d6e
trinitymaharanii <sonheer@gmail.com>
2020-04-07 09:47:06 +0800

    memperbaharui sistem logout

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -8,4 +8,12 @@ class Auth extends Controller
     	$data['judul'] = "Login";
 		$this->view('perpustakaan/login/login',$data);
     }
+    
+    public function logout()
+    {
+    	$_SESSION = [];
+    	session_unset();
+    	session_destroy();
+    	header('Location: '.BASEURL.'/home/page');
+    }
 }
