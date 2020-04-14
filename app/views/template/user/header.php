03f55b6a9cb9be9c414c1b0446512c7db6b12b47
jeniaandini <putujeniaandini@gmail.com>
2020-04-14 08:26:28 +0800

    Create header.php untuk user

---
+++ app/views/template/user/header.php
@@ -0,0 +1,31 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User | Skensa</title>
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css'> 
+	<!-- <link  rel="stylesheet" type="text/css" href="<?= BASEURL ?>/vendor/fontawesome-free/css/all.min.css"> -->
+	<link rel="stylesheet" href="<?= BASEURL ?>/css/style.css">
+	<link rel="stylesheet" type="text/css" href=" <?= BASEURL ?>/css/style_card.css">
+
+</head>
+<body>
+	
+	<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow user-nav">
+		<!-- Topbar Navbar -->
+		<div class="mr-auto">
+			<div class="search">
+				<form>
+					<input type="search" placeholder="Search">
+				</form>
+			</div>
+		</div>
+		<ul class="navbar-nav ml-auto">
+			<div class="topbar-divider d-sm-block"></div>
+			<li class="nav-item">
+				<span class="mr-2 text-gray-600 small">Wahyu Purnama</span>
+				<img class="img-profile rounded-circle" src="<?= BASEURL ?>/img/wahyu.jpg">
+			</li>
+		</ul>
+	</nav>
